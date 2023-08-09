33d0be3d229b0db21ab0ea9735dc2e931f22bce3
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-08-09 17:54:14 +0530

    Aws parameter store

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name = "test"
-  type = "String"
   value = "Hello World"
+  type = "SecureString"
+  key_id = "8f71c404-2eda-4d29-84bf-0a2e0507587a"
 }
