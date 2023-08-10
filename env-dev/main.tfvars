c7bb8b089de78dc63f08212a82b4522a12f81ca1
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-08-10 15:08:49 +0530

    aws ssm parameters

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.lokeshnagireddy.online:8080"},
+  { name = "dev.frontend.cart_url", value = "http://cart-dev.lokeshnagireddy.online:8080"}
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
-  name = "test1"
-  value = "Hello World"
-  type = "SecureString"
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type = "String"
   key_id = "8f71c404-2eda-4d29-84bf-0a2e0507587a"
 }
